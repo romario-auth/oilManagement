27d34cd4b6a2b4cae9687a8c0449cee9b5c7e2d4
Romario de Melo Damasceno <romario.damasceno@freto.com>
2023-04-02 19:07:12 -0300

    Configure data In Memory

--- src/Services/OilManagementMvc/Program.cs
+++ src/Services/OilManagementMvc/Program.cs
@@ -6,9 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Use UseSqlServer
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// UseInMemoryDatabase
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("connectionString");
+});
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();

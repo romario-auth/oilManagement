6bc9f56d360d87fb0efde5bb76d1c229c18ce4bf
Romario de Melo Damasceno <romario.damasceno@freto.com>
2023-04-07 15:15:42 -0300

    Create a EntityBase and ColletionPoint

---
+++ src/Services/OilManagementMvc/Models/CollectPoint.cs
@@ -0,0 +1,13 @@
+﻿using OilManagementMvc.Shared;
+
+namespace OilManagementMvc.Models;
+
+public class CollectPoint : Entity
+{
+    public String? Address { get; set; }
+    public String? City { get; set; }
+    public String? State { get; set; }
+    public String ZipCode { get; set; }
+    public int Number { get; set; }
+    public String Name { get; set; }
+}
---
+++ src/Services/OilManagementMvc/Shared/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace OilManagementMvc.Shared;
+
+public class Entity
+{
+    public Guid Id { get; set; }
+
+    private DateTime createdAt;
+
+    public DateTime GetCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void SetCreatedAt(DateTime value)
+    {
+        createdAt = DateTime.UtcNow;
+    }
+
+    public DateTime UpdatedAt { get; set; }
+}

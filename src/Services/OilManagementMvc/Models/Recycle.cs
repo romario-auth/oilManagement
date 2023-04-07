8d0991d4aaf4cbe7873f0779b22bb6008d3e459a
Romario de Melo Damasceno <romario.damasceno@freto.com>
2023-04-07 17:56:57 -0300

    Create entity Recycle

---
+++ src/Services/OilManagementMvc/Models/Recycle.cs
@@ -0,0 +1,31 @@
+﻿using OilManagementMvc.Shared;
+using System.ComponentModel;
+
+namespace OilManagementMvc.Models;
+
+public class Recycle : Entity
+{
+    public CollectPoint CollectPoint { get; set; }
+    public Guid CollectPointId { get; set; }
+    public TypeRecycle TypeRecycle { get; set; }
+    public int Count { get; set; }
+    public UnitType UnitType { get; set; }
+}
+
+public enum UnitType
+{
+    [Description("Kilogramas")]
+    kilograms = 0,
+
+    [Description("Liters")]
+    Liters = 1,
+
+    [Description("Tons")]
+    Tons = 2
+}
+
+public enum TypeRecycle
+{
+    [Description("Oil Kitchen")]
+    OilKitchen = 0
+}
